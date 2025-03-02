7b424714c3dbdd3ebb72b5d8869abcc574a96367
jinhui66 <1303101041@qq.com>
2025-03-02 17:57:31 +0800

    GCM

--- double_C.py
+++ double_C.py
@@ -2,7 +2,7 @@
 PoolFormer implementation
 """
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 import copy
 import torch
 import torch.nn as nn
@@ -28,6 +28,9 @@ from network.Eyenet import *
 import numpy as np
 from network.DoubleResnet import DoubleResNet
 from network.DoubleResnetShared import DoubleResNetShared
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 
 def to0_1(tensor):
@@ -45,8 +48,8 @@ def k_fold_cross_validation(device, eyes_dataset, epochs, k_fold=5,
     best_acc_overall = 0.
     epoch_acc_dict = {}
     
-    train_metric = Metric_Manager(num_classes=7)
-    valid_metric = Metric_Manager(num_classes=7)
+    train_metric = Metric_Manager(num_classes=1)
+    valid_metric = Metric_Manager(num_classes=1)
     
     # 获取数据集的标签
     # labels = [data[1] for data in eyes_dataset]  # 假设dataset[i]的第2项是label
@@ -66,32 +69,28 @@ def k_fold_cross_validation(device, eyes_dataset, epochs, k_fold=5,
         train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
         eval_dataloader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
         
-        X = torch.zeros(7)
-        Count = 0
-        for (_, _, labels) in train_dataloader:
-            X += torch.sum(labels[:, 1:],dim=0)
-            Count += labels.shape[0]
-            # break
-            # print(X, Count)
-        # X = torch.tensor([100., 100., 100., 100., 100., 100., 100.])
-        print("各疾病总数:", X, Count)
-        X = Count / X - 1
-        print("各疾病损失权重:", X)
-        # X = torch.tensor([1.4978,  4.6430, 12.7381, 12.4971, 16.7538, 34.5077, 14.7007,  2.1147])
+        # X = torch.zeros(1)
+        # Count = 0
+        # for (_, _, labels) in train_dataloader:
+        #     X += torch.sum(labels[:, 3:4],dim=0)
+        #     Count += labels.shape[0]
+        #     # break
+        #     # print(X, Count)
+        # # X = torch.tensor([100., 100., 100., 100., 100., 100., 100.])
+        # print("各疾病总数:", X, Count)
+        # X = Count / X - 1
+        # print("各疾病损失权重:", X)
+        X = torch.tensor([12.5706])
         loss_fn = nn.BCEWithLogitsLoss(pos_weight=X).to(device)  # 损失函数
         # loss_fn = nn.BCEWithLogitsLoss().to(device)
         # model = poolformer_s24(num_classes=8).to(device)
 
-        # model = EyeSeeNet(num_class=7).to(device)
-        # model = ResNet_stage1(BasicBlock, [2, 2, 2, 2], num_classes=2).to(device)
-        # model = DoubleResNet(num_classes=7).to(device)
-        model = DoubleResNetShared(num_classes=7).to(device)
-        
+        model = DoubleResNetShared(num_classes=1).to(device)
         
         optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=1e-7, betas=(0.9, 0.98))
         total_params = sum(p.numel() for p in model.parameters())
         print('总参数个数:{}'.format(total_params))
-        best_loss = 10.
+        best_loss = 100.
         best_epoch = 0
         best_acc = 0.
         best_metrics = {}
@@ -105,20 +104,26 @@ def k_fold_cross_validation(device, eyes_dataset, epochs, k_fold=5,
             train_iterator = tqdm(train_dataloader, desc=f"Training Epoch {e}", unit="batch")
             for batch_idx, (left_images, right_images, labels) in enumerate(train_iterator):
                 left_images, right_images, labels = left_images.to(device), right_images.to(device), labels.to(device)
-
+                # array = left_images[0].permute(1,2,0).cpu().numpy()
+                # plt.imshow(array)  # image_np[0]的形状为 (height, width, channels)
+                # plt.axis('off')  # 不显示坐标轴
+                # plt.title('Visualized Image')
+                # plt.savefig(f'visual/image{batch_idx}.png')
+                
                 optimizer.zero_grad()
 
                 predictions = model(left_images, right_images)
 
-                loss = loss_fn(predictions, labels[:, 1:])
+                loss = loss_fn(predictions, labels[:, 3:4])
                 loss.backward()
                 optimizer.step()
                 total_train_loss += loss.item()
                 
                 predictions = to0_1(predictions)
                 
-
-                train_metric.update(predictions, labels[:, 1:])
+                # print(predictions.shape)
+                train_metric.update(predictions, labels[:, 3:4])
+                                
 
 
             train_accuracy, train_recall, train_precision, train_specificity = train_metric.get_metrics()
@@ -142,7 +147,7 @@ def k_fold_cross_validation(device, eyes_dataset, epochs, k_fold=5,
                     predictions = model(left_images, right_images)
                     
                     # predictions = torch.sigmoid(predictions)
-                    loss = loss_fn(predictions, labels[:, 1:])
+                    loss = loss_fn(predictions, labels[:, 3:4])
                     total_eval_loss += loss.item()
                     
                     # print(predictions[:, 0], labels[:, 0])
@@ -150,7 +155,7 @@ def k_fold_cross_validation(device, eyes_dataset, epochs, k_fold=5,
                     
                     # _, val_predicted = torch.max(predictions.data, 1)
 
-                    valid_metric.update(predictions, labels[:, 1:])
+                    valid_metric.update(predictions, labels[:, 3:4])
                     
                 valid_accuracy, valid_recall, valid_precision, valid_specificity = valid_metric.get_metrics()
                 average_accuracy = np.average(valid_accuracy)
@@ -160,7 +165,7 @@ def k_fold_cross_validation(device, eyes_dataset, epochs, k_fold=5,
             if total_eval_loss < best_loss:
                 best_loss = total_eval_loss
                 best_epoch = e
-                torch.save(model.state_dict(), f'./checkpoint/share_fold{fold}.pth')
+                torch.save(model.state_dict(), f'./checkpoint/double_G_image{fold}.pth')
 
                 # torch.save(model.state_dict(), checkpoint_dir + f'/{best_result_model_path}_fold{fold}.pth')
                 
@@ -181,9 +186,9 @@ def k_fold_cross_validation(device, eyes_dataset, epochs, k_fold=5,
             f.write(f"Fold {fold} Best Epoch: {best_epoch}, Best Val Acc: {best_acc:.3f} \n")  # 在最后添加一个换行符以保持格式整洁
         epoch_acc_dict[fold] = best_acc
 
-        # if best_acc > best_acc_overall:
-        #     best_acc_overall = best_acc
-        # break
+        if best_acc > best_acc_overall:
+            best_acc_overall = best_acc
+        break
 
     end = time.time()
     print(f"Cross-validation result:{epoch_acc_dict}")
@@ -211,7 +216,7 @@ if __name__ == '__main__':
         transforms.Resize((image_size, image_size)),
         transforms.RandomHorizontalFlip(0.5),
         transforms.RandomRotation(45),
-        # transforms.RandomAdjustSharpness(1.3, 1),
+        transforms.RandomAdjustSharpness(1.3, 1),
         transforms.ToTensor(),
         transforms.Normalize(mean, std)
         ]),
@@ -226,6 +231,8 @@ if __name__ == '__main__':
         transforms.Normalize(mean, std)
         ])
     }
+    # data_dir = "/data3/wangchangmiao/jinhui/eye/result_seg"
+    data_dir = "/data3/wangchangmiao/jinhui/eye/ietk_Enhanced"
     # 初始化自定义数据集
     dataset = DoubleEyesDataset(csv_file="./double_valid_data.csv",
                             img_prefix=args.data_dir,
@@ -237,7 +244,7 @@ if __name__ == '__main__':
     # 训练的总轮数
     EPOCH = 500
     epoch = 0
-    otuput_file = "double_ietk256.txt"
+    otuput_file = "double_C.txt"
     k_fold_cross_validation(device, dataset, EPOCH, k_fold=args.k_split_value,
                             batch_size=args.batch_size, workers=2, print_freq=1, checkpoint_dir=model_dir,
                             best_result_model_path="model", total_transform=train_validation_test_transform)
